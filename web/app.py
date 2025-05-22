8f2b1df30420230c23c06e587d95dc483e66bccd
James Coglan <james@neighbourhood.ie>
2025-05-22 17:19:02 +0100

    feat: Provide a UI for updating the refs via the admin app

--- web/app.py
+++ web/app.py
@@ -1,62 +1,39 @@
+import subprocess
 from flask import Flask, redirect, url_for, abort, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/cmd")
 def index():
-	return """
-	<h1>Poppler CI Admin</h1>
-	<ul>
-		<li>
-			<a href="/start-ci">Start CI for a Merge Request</a>
-		</li>
-		<li>
-			<a href="/show-results">See Result Diffs</a>
-		</li>
-	</ul>
-	"""
+    return """
+    <h1>Poppler CI Admin</h1>
+    <ul>
+        <li>
+            <a href="/cmd/update-refs">Update refs from a build</a>
+        </li>
+    </ul>
+    """
 
-@app.post("/start-ci")
-def start_ci_post():
-	if handle_start_ci(request):
-		return redirect(url_for("start_ci_get", started="yes"))
-	else:
-		abort(500)
+@app.get("/cmd/update-refs")
+def update_refs_form():
+    return """
+    <h1>Update refs from a build</h1>
 
-@app.get("/start-ci")
-def start_ci_get():
-	yes = request.args.get("started", default="no")
-	if yes == "yes":
-		success_message = "Build Started<br>"
-	else:
-		success_message = ""
-	return success_message + """
-		<form method="post">
-			<input type="text" name="branch">
-			<button type="submit">Submit</button>
-		</form>
-	"""
+    <form method="post" action="/cmd/update-refs">
+        <p><label for="build_number">Builder number</label>
+            <input type="number" id="build_number" name="build_number"></p>
+        <p><input type="submit" value="Go"></p>
+    </form>
+    """
 
-@app.get("/test")
-def test_get():
-	return url_for("start")
-
-def handle_start_ci(request):
-	branch = request.form["branch"]
-	return start_ci_for_branch(branch)
-
-def start_ci_for_branch(branch):
-	# do the thing
-	return True
-
-@app.get("/show-results/<id>")
-def show_results_get(id):
-	if id:
-		# load result by id from results dir and display
-		return True # dummy code to make the parser happy
-	else:
-		# list all results:
-		# - open result dir
-		# - for each entry:
-		#   - link entryname -> /show-results/entry-id
-		return False # dumy code to make the parser happy
+@app.post("/cmd/update-refs")
+def update_refs_submit():
+    build_number = request.form["build_number"]
+    build_dir = f"/buildbot/outputs/poppler-builder/build-{build_number}"
+    subprocess.run([
+        "/buildbot/refs/update",
+        "--refs-path", "/buildbot/refs",
+        "--poppler-path", "/tmp",
+        "--from-dir", build_dir
+    ])
+    return redirect(url_for("index"))
