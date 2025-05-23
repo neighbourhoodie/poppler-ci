a328d7cf1847e539b1ffc402a7e87c2e4a5bdea5
James Coglan <james@neighbourhood.ie>
2025-05-23 15:15:06 +0100

    feat: Admin action to remove all but the last N builds

--- web/app.py
+++ web/app.py
@@ -1,7 +1,13 @@
+import os
+import shutil
 import subprocess
-from flask import Flask, abort, render_template, request
+
+from flask import Flask, abort, redirect, render_template, request, url_for
 
 PREFIX = "/cmd"
+
+REFS_PATH = "/buildbot/refs"
+OUTPUTS_PATH = "/buildbot/outputs/poppler-builder"
 
 app = Flask(__name__)
 
@@ -16,11 +22,11 @@ def update_refs_form():
 @app.post(PREFIX + "/update-refs")
 def update_refs_submit():
     build_number = request.form["build_number"]
-    build_dir = f"/buildbot/outputs/poppler-builder/build-{build_number}"
+    build_dir = os.path.join(OUTPUTS_PATH, f"build-{build_number}")
 
     proc = subprocess.run([
         "/buildbot/refs/update",
-        "--refs-path", "/buildbot/refs",
+        "--refs-path", REFS_PATH,
         "--poppler-path", "/tmp",
         "--from-dir", build_dir
     ], capture_output=True)
@@ -29,3 +35,26 @@ def update_refs_submit():
         build_dir=build_dir,
         stdout=proc.stdout.decode("utf-8"),
         stderr=proc.stderr.decode("utf-8"))
+
+@app.get(PREFIX + "/cleanup")
+def cleanup_builds_form():
+    return render_template("cleanup_builds_form.html")
+
+@app.post(PREFIX + "/cleanup")
+def cleanup_builds_submit():
+    max_builds = int(request.form["max_builds"])
+    builds = os.listdir(OUTPUTS_PATH)
+
+    def parse_dirname(dirname):
+        _, num = dirname.split('-', 1)
+        num = int(num)
+        return (num, dirname)
+
+    parsed = [parse_dirname(dir) for dir in builds]
+    parsed.sort(reverse=True)
+    to_remove = [dir for (_, dir) in parsed][max_builds:]
+
+    for dir in to_remove:
+        shutil.rmtree(os.path.join(OUTPUTS_PATH, dir))
+
+    return redirect(url_for("index"))
